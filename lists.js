70f605077f7f925ef8508af8b7dc403c5c8003ba
Daniel Samuel <dansam5000@gmail.com>
2021-10-25 11:38:23 +0100

    soln: implement mapList logic func

--- lists.js
+++ lists.js
@@ -1,8 +1,8 @@
 class Link {
-    constructor(value, next = null) {
-        this.value = value;
-        this.next = next;
-    }
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 const emptyList = new Link(null);
@@ -12,14 +12,26 @@ const first = (list) => list.value;
 const rest = (list) => list.next;
 
 const listToStringH = (list) => {
-    if (isEmpty(list)) return '';
-    if (isEmpty(rest(list))) return list.value.toString();
-    const str = first(list) + ', ' + listToStringH(rest(list));
-    return str;
-}
+  if (isEmpty(list)) return '';
+  if (isEmpty(rest(list))) return list.value.toString();
+  const str = first(list) + ', ' + listToStringH(rest(list));
+  return str;
+};
 
-const listToString = (list) => ('[' + listToStringH(list) + ']');
+const mapList = (list, func) => {
+  if (isEmpty(list)) return emptyList;
+  const restResult = mapList(rest(list), func);
+  const firstResult = func(first(list));
+  return cons(firstResult, restResult);
+};
+
+const listToString = (list) => '[' + listToStringH(list) + ']';
 
 const list = cons(1, cons(2, cons(3, emptyList)));
 
 console.log(listToString(list));
+console.log(
+  mapList(list, (val) => {
+    return val * 10;
+  })
+);
